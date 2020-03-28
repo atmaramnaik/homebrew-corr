7d5c8f3d889b7dec66ed0c57b4c71a2d994276d6
atmaram <atmaram@Atmaram-Macbook.local>
2020-03-28 12:05:11 +0530

    First release

---
+++ Formula/corr.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Corr < Formula
+  desc "Corr Client for connecting and dealing with corr servers"
+  homepage "https://github.com/atmaramnaik/corr"
+  url "https://github.com/atmaramnaik/corr/files/4396720/corr-mac.tar.gz"
+  sha256 "4041bc6baff44db316aaead2c108f91a92e85ddeea7bb960b8add297aaa233b2"
+  version "0.0.1"
+
+  def install
+    bin.install "corr"
+  end
+end
