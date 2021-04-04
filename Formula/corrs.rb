992c4017cd6238c4cf343423fecd4f1dafe32fcb
Atmaram Naik <21354507+atmnk@users.noreply.github.com>
2021-04-04 18:46:07 +0530

    Update corrs.rb

--- Formula/corrs.rb
+++ Formula/corrs.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Corrs < Formula
   desc "Corrs Server for connecting and dealing with corr servers"
   homepage "https://github.com/atmaramnaik/corr"
