e9ace0b635d22ca32c1426ca9cbeacb1ccdef219
Atmaram Naik <21354507+atmnk@users.noreply.github.com>
2021-04-04 18:46:56 +0530

    Update corr.rb

--- Formula/corr.rb
+++ Formula/corr.rb
@@ -1,5 +1,5 @@
 class Corr < Formula
-  desc "Corr Client for connecting and dealing with corr servers"
+  desc "Client for connecting and dealing with corr servers"
   homepage "https://github.com/atmaramnaik/corr"
   url "https://github.com/atmnk/corr/releases/download/v0.0.32/corr.tar.gz"
   sha256 "dcd3c57c904feef3a515eb864cb59a5fe353b60b8af0da8c4e6ede7f47f1a86a"
