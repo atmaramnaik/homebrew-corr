e07969e26ffdd7badc09bd6a11ccc50aa933ac2f
Atmaram Naik <21354507+atmnk@users.noreply.github.com>
2021-08-27 20:45:32 +0530

    Update corr.rb

--- Formula/corr.rb
+++ Formula/corr.rb
@@ -4,7 +4,6 @@ class Corr < Formula
   url "https://github.com/atmnk/corr/releases/download/v0.1.4/corr.tar.gz"
   version "0.1.4"
   sha256 "2f287b08e20c742f162539ed4bc1f456f01e4531565b9bed6b87bb199cb9677a"
-
   def install
     bin.install "corr"
   end
