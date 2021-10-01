a7c9b0d63744abe3db2a051faddd03f653ffa43b
Atmaram Naik <21354507+atmnk@users.noreply.github.com>
2021-10-01 03:34:06 -0700

    Bump up corrs formula to v0.1.10 version

--- Formula/corrs.rb
+++ Formula/corrs.rb
@@ -1,9 +1,9 @@
 class Corrs < Formula
   desc "Server for connecting and dealing with corr servers"
   homepage "https://github.com/atmaramnaik/corr"
-  url "https://github.com/atmnk/corr/releases/download/v0.1.9/corrs.tar.gz"
-  version "0.1.9"
-  sha256 "4ca18b6aea5ed3891235c8350bf11604b471cd93266ed3f1fd2e1d968ea7c95d"  
+  url "https://github.com/atmnk/corr/releases/download/v0.1.10/corrs.tar.gz"
+  version "0.1.10"
+  sha256 "acb768d1b4e02b571fc8b102200197fc06b0fa2cbdb0b18a4b94a51e68af9e2b"  
   def install
     bin.install "corrs"
     sd.mkpath
