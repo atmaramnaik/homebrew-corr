148587da3a037f574541a97ea6d8a13c5d7e4aec
Atmaram Naik <21354507+atmnk@users.noreply.github.com>
2022-04-15 12:58:32 -0700

    Bump up corr formula to v0.1.28 version

--- Formula/corr.rb
+++ Formula/corr.rb
@@ -1,9 +1,9 @@
 class Corr < Formula
   desc "Client for connecting and dealing with corr servers"
   homepage "https://github.com/atmaramnaik/corr"
-  url "https://github.com/atmnk/corr/releases/download/v0.1.27/corr.tar.gz"
-  version "0.1.27"
-  sha256 "211eb24bc4db3830933f557957af93a1b06eafdcf48e3a9a691e155fc648b642"
+  url "https://github.com/atmnk/corr/releases/download/v0.1.28/corr.tar.gz"
+  version "0.1.28"
+  sha256 "dec868072f4358035eb3b698f0de6bee2dee9d300e92624dd61a176f7190955c"
   def install
     bin.install "corr"
   end
