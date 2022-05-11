581497f07f121911240f53dfb1babfbb6fc5f9af
ATM <naik.atmaram@gmail.com>
2022-05-11 18:52:58 +0530

    ATM | add beta formula

---
+++ Formula/corrbeta.rb
@@ -0,0 +1,10 @@
+class CorrBeta < Formula
+  desc "Client for connecting and dealing with corr servers"
+  homepage "https://github.com/atmaramnaik/corr"
+  url "https://github.com/atmnk/corr/releases/download/v0.2.1/corr.tar.gz"
+  version "0.2.1"
+  sha256 "23f717619b28e6b4360f8c39dc9fbd14429976fe503ef7df2a4bf77440ef83f9"
+  def install
+    bin.install "corr"
+  end
+end
